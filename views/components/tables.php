38dd45e4395e74762bd9085bc11924607e2b6872
kingjohnny <johnny@thinktomorrow.be>
2017-03-07 21:40:32 +0100

    Tables

--- views/components/tables.php
+++ views/components/tables.php
@@ -1,4 +1,7 @@
-<table class="table">
+<section class="stack-l typography">
+<h1>Tables</h1>
+<hr>
+<table class="stack table">
   <thead>
     <tr>
       <th>#</th>
@@ -28,3 +31,96 @@
     </tr>
   </tbody>
 </table>
+<table class="stack table table-default">
+  <thead>
+    <tr>
+      <th>#</th>
+      <th>First Name</th>
+      <th>Last Name</th>
+      <th>Username</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Larry</td>
+      <td>the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
+
+<table class="stack table table-primary">
+  <thead>
+    <tr>
+      <th>#</th>
+      <th>First Name</th>
+      <th>Last Name</th>
+      <th>Username</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Larry</td>
+      <td>the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
+
+<table class="stack table table-secundary">
+  <thead>
+    <tr>
+      <th>#</th>
+      <th>First Name</th>
+      <th>Last Name</th>
+      <th>Username</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Larry</td>
+      <td>the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
+</section>
--- views/components/thumbnails.php
+++ views/components/thumbnails.php
@@ -3,28 +3,28 @@
   <hr>
 <div class="row gutter">
   <div class="column">
-    <a href="#" class="thumbnail">
-      <img src="http://placehold.it/135x150" alt="">
+    <a href="#" class="thumbnail raised">
+      <img src="http://placehold.it/300x300" alt="">
     </a>
   </div>
   <div class="column">
     <a href="#" class="thumbnail">
-      <img src="http://placehold.it/135x150" alt="">
+      <img src="http://placehold.it/300x300" alt="">
     </a>
   </div>
   <div class="column">
     <a href="#" class="thumbnail">
-      <img src="http://placehold.it/135x150" alt="">
+      <img src="http://placehold.it/300x300" alt="">
     </a>
   </div>
   <div class="column">
     <a href="#" class="thumbnail">
-      <img src="http://placehold.it/135x150" alt="">
+      <img src="http://placehold.it/300x300" alt="">
     </a>
   </div>
   <div class="column">
     <a href="#" class="thumbnail">
-      <img src="http://placehold.it/135x150" alt="">
+      <img src="http://placehold.it/300x300" alt="">
     </a>
   </div>
 </div>
