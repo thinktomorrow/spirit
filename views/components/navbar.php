0e26020d8add4b578418e1779bef755ac0e75126
kingjohnny <johnny@thinktomorrow.be>
2017-03-08 23:00:29 +0100

    navpills

--- views/components/navbar.php
+++ views/components/navbar.php
@@ -4,22 +4,65 @@
   <div class="row">
   <nav class="navbar nav-pills nav-pills-primary">
     <ul>
-      <li class="squished inline"><a href="#">Action</a></li>
-      <li class="squished inline"><a href="#">Another action</a></li>
-      <li class="squished inline"><a href="#">Something else here</a></li>
+      <li class="squished inline-s"><a href="#">Action</a></li>
+      <li class="squished inline-s"><a href="#">Another action</a></li>
+      <li class="squished inline-s"><a href="#">Something else here</a></li>
+    </ul>
+  </nav>
+  <nav class="navbar nav-pills nav-pills-secundary">
+    <ul>
+      <li class="squished inline-s"><a href="#">Action</a></li>
+      <li class="squished inline-s"><a href="#">Another action</a></li>
+      <li class="squished inline-s"><a href="#">Something else here</a></li>
     </ul>
   </nav>
 </div>
-  <h1>Menu</h1>
+  <h2>Navigation pills with icons</h2>
+  <div class="row">
+  <nav class="navbar column-12 nav-pills nav-pills-primary">
+    <ul>
+      <li class="squished inline-s">
+        <a href="#">
+          <i class="icon icon-pen"></i>
+          Action
+        </a>
+      </li>
+      <li class="squished inline-s">
+        <a href="#">
+          <i class="icon icon-lock"></i>
+          Action
+        </a>
+      </li>
+    </ul>
+  </nav>
+  <nav class="navbar column-12 nav-pills nav-pills-secundary">
+    <ul>
+      <li class="squished inline-s">
+        <a href="#">
+          <i class="icon icon-pen"></i>
+          Action
+        </a>
+      </li>
+      <li class="squished inline-s">
+        <a href="#">
+          <i class="icon icon-lock"></i>
+          Action
+        </a>
+      </li>
+    </ul>
+  </nav>
+</div>
+
+
+  <h1>Dropdown</h1>
   <hr>
   <nav class="navbar navbar-default">
     <ul class="nav navbar-nav">
-      <li class="active"><a href="#">Link</a></li>
-      <li><a href="#">Link</a></li>
-      <li class="dropdown">
-        <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-expanded="false">Dropdown
-          <b class="caret"></b></a>
-          <ul class="dropdown-menu dropdown-menu-right">
+      <li class="inline-xs"><a href="#" class="btn btn-s btn-primary btn-round">Link</a></li>
+      <li class="inline-xs"><a href="#" class="btn btn-s btn-primary btn-round">Link</a></li>
+      <li class="inline-xs dropdown">
+        <a href="#" class="dropdown-toggle btn btn-s btn-primary btn-round">Dropdown</a>
+          <ul class="dropdown-menu">
             <li class="dropdown-header">Dropdown header</li>
             <li><a href="#">Action</a></li>
             <li><a href="#">Another action</a></li>
