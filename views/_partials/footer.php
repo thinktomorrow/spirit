29ef877952e42a02f7cf695089ba7b9ba3fb4693
kingjohnny <johnny@thinktomorrow.be>
2017-10-13 17:02:23 +0200

    Changed inputs

--- views/_partials/footer.php
+++ views/_partials/footer.php
@@ -1,82 +1,78 @@
 </main>
 <footer class="inset">
-  &copy; <?php
-  date_default_timezone_set('UTC');
-  echo date('Y')
-  ?> - Spirit
+    &copy; <?php
+    date_default_timezone_set('UTC');
+    echo date('Y')
+    ?> - Spirit
 </footer>
 
 <script type="text/javascript" src="assets/js/jquery.3.2.1.min.js"></script>
 <script>
 $(function() {
-  // SCROLL TO ID
-  $('a[href*=""]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: target.offset().top - 50,
-        }, 2000);
-        return false;
-      }
-    }
-  });
+    // SCROLL TO ID
+    $('a[href*=""]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top - 50,
+                }, 2000);
+                return false;
+            }
+        }
+    });
 
-  // Clone your html into a pre box
-  // !important: You can't skip a number because then the loop stops working.
-  // and all the id's needs to be in the dom to be recognized by the function
-  function cloneHtml(){
-    $('[id^=clone-]').each(function (i) {
-      var cloneDiv = $('#clone-'+ i).html();
-      // var stripFromTag = cloneDiv.replace(/\/</,'&lt;',/\/>/,'&gt;');
-      $('#code-'+ i).text(cloneDiv);
+    // Clone your html into a pre box
+    // !important: You can't skip a number because then the loop stops working.
+    // and all the id's needs to be in the dom to be recognized by the function
+    function cloneHtml(){
+        $('[id^=clone-]').each(function (i) {
+            var cloneDiv = $('#clone-'+ i).html();
+            var stripFromTag = cloneDiv.replace(/\/</,'&lt;',/\/>/,'&gt;');
+            $('#code-'+ i).text(cloneDiv);
+
+        });
+    }
+    cloneHtml();
+
+    // BRING IN THE COLORS
+
+    // Function to turn rgb to hex
+    function rgb2hex(orig){
+        var rgb = orig.replace(/\s/g,'').match(/^rgba?\((\d+),(\d+),(\d+)/i);
+        return (rgb && rgb.length === 4) ? "#" +
+        ("0" + parseInt(rgb[1],10).toString(16)).slice(-2) +
+        ("0" + parseInt(rgb[2],10).toString(16)).slice(-2) +
+        ("0" + parseInt(rgb[3],10).toString(16)).slice(-2) : orig;
+    };
+
+
+
+    var colorArray=[];
+
+    $('.color-block > [class^=bg-]').each(function (i,j) {
+        var rgb = $(this).css('background-color');
+        var hex = rgb2hex($(this).css('background-color'));
+
+        $('.rgbCode-'+i).text( rgb );
+        $('.hexCode-'+i).text( hex );
 
     });
-  }
-  cloneHtml();
 
-  // BRING IN THE COLORS
+    // TABS
+    $('#example .panel-tabs li').click(function(){
+        var nav_tabs = $('#example .panel-tabs li');
+        var panel_tabs = $('.tab');
+        var tab_id = $(this).attr('data-tab');
 
-  // Function to turn rgb to hex
-  function rgb2hex(orig){
-   var rgb = orig.replace(/\s/g,'').match(/^rgba?\((\d+),(\d+),(\d+)/i);
-   return (rgb && rgb.length === 4) ? "#" +
-    ("0" + parseInt(rgb[1],10).toString(16)).slice(-2) +
-    ("0" + parseInt(rgb[2],10).toString(16)).slice(-2) +
-    ("0" + parseInt(rgb[3],10).toString(16)).slice(-2) : orig;
-  };
+        nav_tabs.removeClass('active');
+        panel_tabs.removeClass('active');
 
 
-
-  var colorArray=[];
-
-  $('.color-block > [class^=bg-]').each(function (i,j) {
-      var rgb = $(this).css('background-color');
-      var hex = rgb2hex($(this).css('background-color'));
-
-      $('.rgbCode-'+i).text( rgb );
-      $('.hexCode-'+i).text( hex );
-
-  });
-
-
-
-
-
-  // TABS
-  $('#example .panel-tabs li').click(function(){
-    var nav_tabs = $('#example .panel-tabs li');
-    var panel_tabs = $('.tab');
-    var tab_id = $(this).attr('data-tab');
-
-    nav_tabs.removeClass('active');
-    panel_tabs.removeClass('active');
-
-
-    $(this).addClass('active');
-    $("#"+tab_id).toggleClass('active');
-  })
+        $(this).addClass('active');
+        $("#"+tab_id).toggleClass('active');
+    })
 
 });
 
--- views/components/inputs.php
+++ views/components/inputs.php
@@ -1,87 +1,91 @@
 <section class="stack-l forms" id="forms">
-  <h1>Form elements</h1>
-  <hr>
+    <h1>Form elements</h1>
+    <hr>
 
-  <h2>Input fields</h2>
-  <div class="input-group stack" id="clone-12">
-    <label for="text">Text Input</label>
-    <input id="text" class="input inset-s" placeholder="Text input" type="text" required>
-    <small class="caption">Note about this field</small>
-  </div>
-  <div class="input-group stack valid">
-    <label for="text">Valid Input</label>
-    <input id="text" class="input inset-s" placeholder="Text input" type="text" required>
-    <small class="caption">Note about this field</small>
-  </div>
-  <div class="input-group stack error">
-    <label for="text">Error input</label>
-    <input id="text" class="input inset-s" placeholder="Text input" type="text" required>
-    <small class="caption">Note about this field</small>
-  </div>
-  <pre class="row code-box" id="code-12"></pre>
-
-  <h2>Prepend with icons</h2>
-  <div class="input-group stack" id="clone-13">
-    <label for="text">Text Input</label>
-    <div class="input-group-prefix">
-      <span class="input-prefix"><i class="icon icon-envelope-o"></i></span>
-      <input id="text" class="input inset-s" placeholder="Text input" type="email" required>
+    <h2>Input fields</h2>
+    <div class="input-group stack" id="clone-12">
+        <label for="text">Text Input</label>
+        <input id="text" class="input inset-s" placeholder="Text input" type="text" required>
+        <small class="caption">Note about this field</small>
     </div>
-    <small class="caption">Note about this field</small>
-  </div>
-  <div class="input-group stack valid">
-    <label for="text">Valid Text Input</label>
-    <div class="input-group-prefix ">
-      <span class="input-prefix"><i class="icon icon-envelope-o"></i></span>
-      <input id="text" class="input inset-s" placeholder="Text input" type="email" required>
+    <div class="input-group stack valid">
+        <label for="text">Valid Input</label>
+        <input id="text" class="input inset-s" placeholder="Text input" type="text" required>
+        <small class="caption">Note about this field</small>
     </div>
-    <small class="caption">Note about this field</small>
-  </div>
-  <div class="input-group stack error">
-    <label for="text">Error Text Input</label>
-    <div class="input-group-prefix">
-      <span class="input-prefix"><i class="icon icon-envelope-o"></i></span>
-      <input id="text" class="input inset-s" placeholder="Text input" type="email" required>
+    <div class="input-group stack error">
+        <label for="text">Error input</label>
+        <input id="text" class="input inset-s" placeholder="Text input" type="text" required>
+        <small class="caption">Note about this field</small>
     </div>
-    <small class="caption">Note about this field</small>
-  </div>
-  <pre class="row code-box" id="code-13"></pre>
+    <pre class="row code-box" id="code-12"></pre>
+
+    <h2>Prepend with icons</h2>
+    <div class="input-group stack" id="clone-13">
+        <label for="text">Text Input</label>
+        <div class="input-group-prefix">
+            <span class="input-prefix"><i class="icon icon-envelope"></i></span>
+            <input id="text" class="input inset-s" placeholder="Text input" type="email" required>
+        </div>
+        <small class="caption">Note about this field</small>
+    </div>
+    <div class="input-group stack valid">
+        <label for="text">Valid Text Input</label>
+        <div class="input-group-prefix ">
+            <span class="input-prefix"><i class="icon icon-envelope"></i></span>
+            <input id="text" class="input inset-s" placeholder="Text input" type="email" required>
+        </div>
+        <small class="caption">Note about this field</small>
+    </div>
+    <div class="input-group stack error">
+        <label for="text">Error Text Input</label>
+        <div class="input-group-prefix">
+            <span class="input-prefix"><i class="icon icon-envelope"></i></span>
+            <input id="text" class="input inset-s" placeholder="Text input" type="email" required>
+        </div>
+        <small class="caption">Note about this field</small>
+    </div>
+    <pre class="row code-box" id="code-13"></pre>
 
 
-  <h2>Other Form Elements</h2>
-  <div class="input-group stack">
-    <label for="password">Password</label>
-    <input id="password" class="input inset-s" placeholder="*****" type="password" required>
-  </div>
-  <div class="input-group stack">
-    <label for="number">Number</label>
-    <input id="number" class="input inset-s" placeholder="---- / -- . -- . --" type="phone" required>
-  </div>
-  <div class="input-group stack">
-    <label for="search">Search</label>
-    <input id="search" class="input inset-s" placeholder="Search" type="search" data-icon="icon-search" required>
-  </div>
-  <div class="input-group stack">
-    <label for="email">Email Address</label>
-    <input id="email" class="input inset-s" type="email" placeholder="you@example.com" required>
-  </div>
-  <div class="input-group stack">
-    <label for="email">Textarea</label>
-    <textarea rows="8" cols="80" id="message" class="input inset-s" required></textarea>
-  </div>
+    <h2>Other Form Elements</h2>
+    <div class="input-group stack">
+        <label for="password">Password</label>
+        <input id="password" class="input inset-s" placeholder="*****" type="password" required>
+    </div>
+    <div class="input-group stack">
+        <label for="number">Number</label>
+        <input id="number" class="input inset-s" placeholder="---- / -- . -- . --" type="phone" required>
+    </div>
+    <div class="input-group stack">
+        <label for="search">Search</label>
+        <div class="search-wrapper">
+            <input id="search" class="input inset-s" placeholder="Search" type="search" data-icon="icon-search" required>
+        </div>
+    </div>
+    <div class="input-group stack">
+        <label for="email">Email Address</label>
+        <input id="email" class="input inset-s" type="email" placeholder="you@example.com" required>
+    </div>
+    <div class="input-group stack">
+        <label for="email">Textarea</label>
+        <textarea rows="8" cols="80" id="message" class="input inset-s" required></textarea>
+    </div>
 
-  <div class="input-group stack">
-    <select>
-      <option selected>Open this select menu</option>
-      <option>Option 1</option>
-      <option>Option 2</option>
-    </select>
-  </div>
+    <div class="input-group stack">
+        <div class="select-wrapper">
+            <select>
+                <option selected>Open this select menu</option>
+                <option>Option 1</option>
+                <option>Option 2</option>
+            </select>
+        </div>
+    </div>
 
-  <div class="input-group stack">
-    <label class="custom-file">
-      <input type="file" id="file">
-      <span class="custom-file-input"></span>
-    </label>
-  </div>
+    <div class="input-group stack">
+        <label class="custom-file">
+            <input type="file" id="file">
+            <span class="custom-file-input" data-title="Kies uw bestand" data-button="Browse"></span>
+        </label>
+    </div>
 </section>
