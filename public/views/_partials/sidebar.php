da7e674be34aade50da253ac88da8fcffe18be23
KingJohnny <johnny@thinktomorrow.be>
2018-04-12 17:41:47 +0200

    WIP layout

--- public/views/_partials/sidebar.php
+++ public/views/_partials/sidebar.php
@@ -1,43 +1,44 @@
-<aside class="nav column-2 inset">
-    <div class="header-logo">
-      <a href="?summon=home"><img src="assets/svg/logo.svg" alt="Spirit"></a>
+<aside class="column-2">
+    <div class="header-logo squished-l">
+        <a href="?summon=home"><img src="assets/svg/logo.svg" alt="Spirit"></a>
     </div>
     <!-- PAGES -->
-    Settings
-    <li class="nav__item">
-        <a href="?summon=colors">Colors</a>
-    </li>
-    <li class="nav__item">
-        <a href="?summon=typography">Typography</a>
-    </li>
-    <li class="nav__item">
-        <a href="?summon=icons">Icons</a>
-    </li>
-    <hr>
-    Elements
-    <li class="nav__item">  <a href="?summon=elements#buttons">Buttons</a> </li>
-    <li class="nav__item">  <a href="?summon=elements#labels">Labels</a> </li>
-    <li class="nav__item"> <a href="?summon=elements#forms">Forms/inputs</a></li>
-    <li class="nav__item"> <a href="?summon=elements#toggles">Toggles</a></li>
+    <nav class="squished-l">
+        Settings
+        <li class="nav__item">
+            <a href="?summon=colors">Colors</a>
+        </li>
+        <li class="nav__item">
+            <a href="?summon=typography">Typography</a>
+        </li>
+        <li class="nav__item">
+            <a href="?summon=icons">Icons</a>
+        </li>
+        <hr>
+        Elements
+        <li class="nav__item">  <a href="?summon=elements#buttons">Buttons</a> </li>
+        <li class="nav__item">  <a href="?summon=elements#labels">Labels</a> </li>
+        <li class="nav__item"> <a href="?summon=elements#forms">Forms/inputs</a></li>
+        <li class="nav__item"> <a href="?summon=elements#toggles">Toggles</a></li>
 
-    <li class="nav__item"> <a href="?summon=elements#badges">Badges</a></li>
-    <li class="nav__item"> <a href="?summon=elements#tables">Tables</a></li>
+        <li class="nav__item"> <a href="?summon=elements#badges">Badges</a></li>
+        <li class="nav__item"> <a href="?summon=elements#tables">Tables</a></li>
 
-    <li class="nav__item"> <a href="?summon=elements#navbar">Navigation</a></li>
-    <li class="nav__item"> <a href="?summon=elements#thumbnails">Thumbnails</a></li>
-    <li class="nav__item"> <a href="?summon=elements#panels">Panels</a></li>
-    <li class="nav__item"> <a href="?summon=elements#listgroups">Listgroups</a></li>
+        <li class="nav__item"> <a href="?summon=elements#navbar">Navigation</a></li>
+        <li class="nav__item"> <a href="?summon=elements#thumbnails">Thumbnails</a></li>
+        <li class="nav__item"> <a href="?summon=elements#panels">Panels</a></li>
+        <li class="nav__item"> <a href="?summon=elements#listgroups">Listgroups</a></li>
 
-    <hr>
-    <div class="nav__item stack-s">
-        Components
-        <li>Modals</li>
-        <li>Sidebar</li>
-        <li>  <a href="?summon=components#tabs">Tabs</a></li>
-        <li><a href="?summon=components#notifications">Notifications</a></li>
-    </div>
+        <hr>
+        <div class="nav__item stack-s">
+            Components
+            <li>Modals</li>
+            <li>Sidebar</li>
+            <li>  <a href="?summon=components#tabs">Tabs</a></li>
+            <li><a href="?summon=components#notifications">Notifications</a></li>
+        </div>
 
-
+    </nav>
     <footer class="inset">
         &copy; <?= date('Y') ?> - Spirit
     </footer>
