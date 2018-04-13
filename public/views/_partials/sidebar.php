f0927557c858c9729336ae4f33ab3144833de5f9
KingJohnny <johnny@thinktomorrow.be>
2018-04-13 08:53:23 +0200

    Small fixes

--- public/views/_partials/sidebar.php
+++ public/views/_partials/sidebar.php
@@ -4,40 +4,46 @@
     </div>
     <!-- PAGES -->
     <nav class="squished-l">
-        Settings
-        <li class="nav__item">
-            <a href="?summon=colors">Colors</a>
-        </li>
-        <li class="nav__item">
-            <a href="?summon=typography">Typography</a>
-        </li>
-        <li class="nav__item">
-            <a href="?summon=icons">Icons</a>
-        </li>
-        <hr>
-        Elements
-        <li class="nav__item">  <a href="?summon=elements#buttons">Buttons</a> </li>
-        <li class="nav__item">  <a href="?summon=elements#labels">Labels</a> </li>
-        <li class="nav__item"> <a href="?summon=elements#forms">Forms/inputs</a></li>
-        <li class="nav__item"> <a href="?summon=elements#toggles">Toggles</a></li>
-
-        <li class="nav__item"> <a href="?summon=elements#badges">Badges</a></li>
-        <li class="nav__item"> <a href="?summon=elements#tables">Tables</a></li>
-
-        <li class="nav__item"> <a href="?summon=elements#navbar">Navigation</a></li>
-        <li class="nav__item"> <a href="?summon=elements#thumbnails">Thumbnails</a></li>
-        <li class="nav__item"> <a href="?summon=elements#panels">Panels</a></li>
-        <li class="nav__item"> <a href="?summon=elements#listgroups">Listgroups</a></li>
-
-        <hr>
-        <div class="nav__item stack-s">
-            Components
-            <li>Modals</li>
-            <li>Sidebar</li>
-            <li>  <a href="?summon=components#tabs">Tabs</a></li>
-            <li><a href="?summon=components#notifications">Notifications</a></li>
+        <div class="nav__group stack-s">
+            <span class="nav__head">Settings</span>
+            <ul>
+                <li class="nav__item">
+                    <a href="?summon=colors">Colors</a>
+                </li>
+                <li class="nav__item">
+                    <a href="?summon=typography">Typography</a>
+                </li>
+                <li class="nav__item">
+                    <a href="?summon=icons">Icons</a>
+                </li>
+            </ul>
         </div>
+        <div class="nav__group stack-s">
+            <span class="nav__head">Elements</span>
+            <ul>
+                <li class="nav__item">  <a href="?summon=elements#buttons">Buttons</a> </li>
+                <li class="nav__item">  <a href="?summon=elements#labels">Labels</a> </li>
+                <li class="nav__item"> <a href="?summon=elements#forms">Forms/inputs</a></li>
+                <li class="nav__item"> <a href="?summon=elements#toggles">Toggles</a></li>
 
+                <li class="nav__item"> <a href="?summon=elements#badges">Badges</a></li>
+                <li class="nav__item"> <a href="?summon=elements#tables">Tables</a></li>
+
+                <li class="nav__item"> <a href="?summon=elements#navbar">Navigation</a></li>
+                <li class="nav__item"> <a href="?summon=elements#thumbnails">Thumbnails</a></li>
+                <li class="nav__item"> <a href="?summon=elements#panels">Panels</a></li>
+                <li class="nav__item"> <a href="?summon=elements#listgroups">Listgroups</a></li>
+            </ul>
+        </div>
+        <div class="nav__group stack-s">
+            <span class="nav__head">Components</span>
+            <ul>
+                <li class="nav__item">Modals</li>
+                <li class="nav__item">Sidebar</li>
+                <li class="nav__item"> <a href="?summon=components#tabs">Tabs</a></li>
+                <li class= "nav__item"><a href="?summon=components#notifications">Notifications</a></li>
+            </ul>
+        </div>
     </nav>
     <footer class="inset">
         &copy; <?= date('Y') ?> - Spirit
--- public/views/elements/panels.php
+++ public/views/elements/panels.php
@@ -1,78 +1,90 @@
 <section class="stack-l panels" id="panels">
-  <h1>Panels</h1>
-  <hr>
-<div class="row gutter" id="clone-8">
-  <div class="column">
-    <h2>Default panel</h2>
-    <div class="panel panel-default">
-      <div class="panel-header squished-s">Panel header</div>
-      <div class="panel-body inset-s">
-        Default Panel
-      </div>
-      <div class="panel-footer squished-s">Panel footer</div>
+    <h1>Panels</h1>
+    <hr>
+    <div class="row gutter" id="clone-8">
+        <div class="column">
+            <h2>Default panel</h2>
+            <div class="panel panel-default">
+                <div class="panel-header squished-s">Panel header</div>
+                <div class="panel-body inset-s">
+                    Default Panel
+                </div>
+                <div class="panel-footer squished-s">Panel footer</div>
+            </div>
+        </div>
+        <div class="column">
+            <h2>Raised panel</h2>
+            <div class="panel panel-default panel--raised">
+                <div class="panel-header squished-s">Panel header</div>
+                <div class="panel-body inset-s">
+                    Default Panel Raised
+                </div>
+                <div class="panel-footer squished-s">Panel footer</div>
+            </div>
+        </div>
     </div>
-  </div>
-  <div class="column">
-    <h2>Raised panel</h2>
-    <div class="panel panel-default panel--raised">
-      <div class="panel-header squished-s">Panel header</div>
-      <div class="panel-body inset-s">
-        Default Panel Raised
-      </div>
-      <div class="panel-footer squished-s">Panel footer</div>
-    </div>
-  </div>
-  </div>
-  <pre class="row code-box" id="code-8"></pre>
+    <pre class="row code-box" id="code-8"></pre>
 
-  <h2>Basic panel</h2>
-  <div class="row gutter">
-  <div class="panel panel-primary column">
-    <div class="panel-header squished-s">Panel header</div>
-    <div class="panel-body inset-s">
-      Primary Panel
-    </div>
-    <div class="panel-footer squished-s">Panel footer</div>
-  </div>
+    <h2>Basic panel</h2>
+    <div class="row gutter">
+        <div class="column">
+            <div class="panel panel-primary">
+                <div class="panel-header squished-s">Panel header</div>
+                <div class="panel-body inset-s">
+                    Primary Panel
+                </div>
+                <div class="panel-footer squished-s">Panel footer</div>
+            </div>
+        </div>
 
-  <div class="panel panel-secondary column">
-    <div class="panel-header squished-s">Panel header</div>
-    <div class="panel-body inset-s">
-      secondary Panel
+        <div class="column">
+            <div class="panel panel-secondary">
+                <div class="panel-header squished-s">Panel header</div>
+                <div class="panel-body inset-s">
+                    secondary Panel
+                </div>
+                <div class="panel-footer squished-s">Panel footer</div>
+            </div>
+        </div>
     </div>
-    <div class="panel-footer squished-s">Panel footer</div>
-  </div>
-  </div>
 
-  <h2>Other panels</h2>
-<div class="row gutter">
-  <div class="panel panel-success column">
-    <div class="panel-header squished-s">Panel header</div>
-    <div class="panel-body inset-s">
-      Primary Panel
+    <h2>Other panels</h2>
+    <div class="row gutter">
+        <div class="column">
+            <div class="panel panel-success">
+                <div class="panel-header squished-s">Panel header</div>
+                <div class="panel-body inset-s">
+                    Primary Panel
+                </div>
+                <div class="panel-footer squished-s">Panel footer</div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="panel panel-warning">
+                <div class="panel-header squished-s">Panel header</div>
+                <div class="panel-body inset-s">
+                    Primary Panel
+                </div>
+                <div class="panel-footer squished-s">Panel footer</div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="panel panel-error">
+                <div class="panel-header squished-s">Panel header</div>
+                <div class="panel-body inset-s">
+                    Primary Panel
+                </div>
+                <div class="panel-footer squished-s">Panel footer</div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="panel panel-information">
+                <div class="panel-header squished-s">Panel header</div>
+                <div class="panel-body inset-s">
+                    Primary Panel
+                </div>
+                <div class="panel-footer squished-s">Panel footer</div>
+            </div>
+        </div>
     </div>
-    <div class="panel-footer squished-s">Panel footer</div>
-  </div>
-  <div class="panel panel-warning column">
-    <div class="panel-header squished-s">Panel header</div>
-    <div class="panel-body inset-s">
-      Primary Panel
-    </div>
-    <div class="panel-footer squished-s">Panel footer</div>
-  </div>
-  <div class="panel panel-error column">
-    <div class="panel-header squished-s">Panel header</div>
-    <div class="panel-body inset-s">
-      Primary Panel
-    </div>
-    <div class="panel-footer squished-s">Panel footer</div>
-  </div>
-  <div class="panel panel-information column">
-    <div class="panel-header squished-s">Panel header</div>
-    <div class="panel-body inset-s">
-      Primary Panel
-    </div>
-    <div class="panel-footer squished-s">Panel footer</div>
-  </div>
-</div>
 </section>
